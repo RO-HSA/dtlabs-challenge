807dfa7a6db920812e9576efdaa16a6541de3954
Robert Santos <robertdahora@gmail.com>
2024-05-15 22:18:41 +0000

    feat: add charts component

--- src/components/Charts/index.tsx
+++ src/components/Charts/index.tsx
@@ -1,5 +1,35 @@
+import PageTitle from '@/components/PageTitle'
+import CustomDate from '@/components/CustomDate'
+
+import styles from './charts.module.css'
+
 const Charts = () => {
-  return <div></div>
+  const { chartHeader, filterGroup, filter} = styles
+
+  return (
+    <div>
+      <div className={chartHeader}>
+        <PageTitle marginBottom='4px'>Gráficos de Venda</PageTitle>
+        <CustomDate fontSize='16px' borderBottom='none'/>
+        <div className={filterGroup}>
+          <div className={filter}>
+            <label>Ano de Referência</label>
+            <select>
+              <option value="asc">Ordem Crescente</option>
+              <option value="desc">Ordem Decrescente</option>
+            </select>
+          </div>
+          <div className={filter}>
+            <label>Vendedor</label>
+            <select>
+              <option value="asc">Ordem Crescente</option>
+              <option value="desc">Ordem Decrescente</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Charts
--- src/components/CustomDate/index.tsx
+++ src/components/CustomDate/index.tsx
@@ -3,16 +3,17 @@ import { getCurrentDate } from '@/utils/DateTime'
 import styles from './customDate.module.css'
 
 interface Props {
-  fontSize?: string
-  className?: string
+  fontSize?: string;
+  borderBottom?: string;
+  className?: string;
 }
 
-const CustomDate: FC<Props> = ({ className, fontSize = '32px' }) => {
+const CustomDate: FC<Props> = ({ className, fontSize = '32px', borderBottom = '2px solid var(--border)' }) => {
   const currentDate = getCurrentDate()
 
   return (
     <div className={styles.dateWrapper}>
-      <p style={{ fontSize }} className={[styles.date, className].join(' ')}>
+      <p style={{ fontSize, borderBottom }} className={[styles.date, className].join(' ')}>
         {currentDate}
       </p>
     </div>
